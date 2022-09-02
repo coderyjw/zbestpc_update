84271c9e875aaacc80b8cbd617401ae7669cb7da
叶继伟 <729721503@qq.com>
2022-09-03 03:55:59 +0800

    使用html-webpack-plugin实现自动注入bundle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log("zbestpc");
+console.log("zbestpc2");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    bundle: "./src/index.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[hash].js",
     path: path.resolve(__dirname, "./dist"),
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
